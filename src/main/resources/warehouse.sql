4856d205fc656483bf56bc956480eb91a9427162
Arsen Grigoryan <arsen.grigoryan@iunetworks.am>
2019-03-15 23:05:48 +0400

    edit

--- src/main/resources/warehouse.sql
+++ src/main/resources/warehouse.sql
@@ -16,7 +16,17 @@ VALUES (1, 1),
 
 -- ----------------------------------------------------------------------------------------------------------------
 
+INSERT INTO products (id,name,type,description,count_in_warehouse,count_in_shop,purchase_price,sale_price,expiration_date,
+                      product_code,bar_code)
+VALUES (1, 'spichka', 'lucki', 'haykakan lucki', 10, 5, '100', '110', '2018-02-02', '12345678', '87654321'),
+       (2, 'cello', 'grich', 'sev grich', 10, 5, '120', '130', '2018-02-02', '23456789', '98765432');
 
+
+UPDATE products
+SET count_in_warehouse = count_in_warehouse + '10' ,
+    count_in_shop = count_in_shop + '10'
+WHERE bar_code = '87654321';
+commit ;
 
 
 
